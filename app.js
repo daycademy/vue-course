31a8c217020019e517cfcb1535a7f7cbe3291277
florianwoelki <florianwoelki@gmx.de>
2020-05-07 11:03:19 +0200

    initial commit for lesson 6

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 new Vue({
   el: '#app',
   data: {
+    firstname: '',
+    lastname: '',
     names: ['Florian', 'Gustav', 'Peter'],
     persons: [
       { name: 'Florian', age: 21 },
       { name: 'Gustav', age: 51 },
     ],
+  },
+
+  computed: {
+    fullname() {
+      return `${this.fullname} ${this.lastname}`;
+    },
   },
 });
