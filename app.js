35961775be9fdc349d5fd99334d55e360d0d4419
florianwoelki <florianwoelki@gmx.de>
2020-05-07 10:50:48 +0200

    initial commit lesson 2

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 new Vue({
   el: '#app',
   data: {
-    name: 'Florian',
+    person: {
+      name: 'Florian',
+      height: 189,
+    },
+    website: 'http://google.com/',
+    atag: '<a href="http://google.com">Testing html</a>',
+  },
+
+  methods: {
+    sayHi: function(greeting) {
+      return greeting + ' ' + this.person.name;
+    },
   },
 });
