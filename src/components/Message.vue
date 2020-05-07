64d1359ea210b8c5581ec68fa48a04dd0c5c3e94
florianwoelki <florianwoelki@gmx.de>
2020-05-07 12:29:06 +0200

    initial commit for lesson 15

---
+++ src/components/Message.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h1>{{ content }}</h1>
+    <input v-model="title">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '',
+    };
+  },
+
+  props: {
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+
+  beforeCreate() {
+    console.log('beforeCreate');
+  },
+
+  created() {
+    console.log('created');
+  },
+
+  beforeMount() {
+    console.log('beforeMount');
+  },
+
+  mounted() {
+    console.log('mounted');
+  },
+
+  beforeUpdate() {
+    console.log('beforeUpdate');
+  },
+
+  updated() {
+    console.log('updated');
+  },
+
+  beforeDestroy() {
+    console.log('beforeDestroy');
+  },
+
+  destroyed() {
+    console.log('destroyed');
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,8 @@
     <TheHeader />
     <TheFooter />
     <UserList :users="users" />
+    <Message v-if="show" @hook:mounted="printMounted()" content="Awesome title" />
+    <button @click="show = false">Click me</button>
   </section>
 </template>
 
@@ -10,20 +12,28 @@
 import TheHeader from '../components/TheHeader.vue';
 import TheFooter from '../components/TheFooter.vue';
 import UserList from '../components/UserList.vue';
+import Message from '../components/Message.vue';
 
 export default {
   components: {
     TheHeader,
     TheFooter,
     UserList,
+    Message,
   },
   data() {
     return {
+      show: true,
       users: [
         { name: 'Florian', age: 21, showAge: false },
         { name: 'Peter', age: 54, showAge: false },
       ],
     };
+  },
+  methods: {
+    printMounted() {
+      console.log('mounted hook called');
+    },
   },
 };
 </script>
