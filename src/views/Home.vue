ab41853242cb42978e3e2e6219ea4742cbdc5bf4
florianwoelki <florianwoelki@gmx.de>
2020-05-26 08:50:26 +0200

    initial commit for lesson 21

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,10 @@
       </template>
       <p slot="subtitle">Subtitle</p>
     </Map>
+    <input v-model="inputText">
+    <p v-if="showFeedback">Text ist > 5</p>
+    <h2>{{ counter }}</h2>
+    <button @click="counter++">Add 1</button>
     <UserList :users="users"></UserList>
     <AddUserButton @new-user="addUser" />
   </section>
@@ -29,8 +33,19 @@ export default {
     AddUserButton,
     Map,
   },
+  watch: {
+    counter(newVal, oldVal) {
+      console.log(`new: ${newVal}, old: ${oldVal}`);
+    },
+    inputText(newVal) {
+      this.showFeedback = newVal.length > 5;
+    },
+  },
   data() {
     return {
+      showFeedback: false,
+      inputText: '',
+      counter: 0,
       users: [
         { name: 'Florian', age: 21, showAge: false },
         { name: 'Peter', age: 54, showAge: false },
