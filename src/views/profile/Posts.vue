d1de904bf0d1975d6ccfb94c51625da995ff9edc
florianwoelki <florianwoelki@gmx.de>
2020-08-21 09:08:46 +0200

    initial commit for lesson 30

--- src/views/profile/Posts.vue
+++ src/views/profile/Posts.vue
@@ -1,3 +1,42 @@
 <template>
-  <h1>Posts</h1>
+  <div>
+    <h1>Posts</h1>
+    <h6>{{ filteredPosts }}</h6>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      userData: [],
+    };
+  },
+  computed: {
+    filteredPosts() {
+      const filter = this.$route.query.sort;
+      if (!filter) {
+        return this.userData;
+      }
+      const filteredPosts = this.userData;
+      return filteredPosts.sort((a, b) => {
+        if (a.title < b.title) {
+          return filter === 'asc' ? -1 : 1;
+        }
+        if (a.title > b.title) {
+          return filter === 'asc' ? 1 : -1;
+        }
+        return 0;
+      });
+    },
+  },
+  created() {
+    const userId = this.$route.params.id;
+    fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+      .then((response) => response.json())
+      .then((response) => {
+        this.userData = response;
+      });
+  },
+};
+</script>
