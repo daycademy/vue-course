349d25bd5effa4eedf3410073477255a6ebf6c25
florianwoelki <florianwoelki@gmx.de>
2020-09-11 08:12:18 +0200

    initial commit for lesson 33

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,5 +6,20 @@
 
 <script>
 export default {
+  beforeRouteEnter(to, from, next) {
+    console.log(`beforeRouteEnter to: ${to.path}`);
+    console.log(`beforeRouteEnter from: ${from.path}`);
+    next();
+  },
+  beforeRouteLeave(to, from, next) {
+    console.log(`beforeRouteLeave to: ${to.path}`);
+    console.log(`beforeRouteLeave from: ${from.path}`);
+    next();
+  },
+  beforeRouteUpdate(to, from, next) {
+    console.log(`beforeRouteUpdate to: ${to.path}`);
+    console.log(`beforeRouteUpdate from: ${from.path}`);
+    next();
+  },
 };
 </script>
